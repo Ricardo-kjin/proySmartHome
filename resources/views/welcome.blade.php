f738b9b54dc97f359b53ce388be46352408e02a2
Rikrdo <rikrdoramirez1993@gmail.com>
2023-10-17 10:49:52 -0400

    arreglo de pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,16 @@
                     </svg>
                 </div>
 
-                <div class="mt-16">
+                <div class="flex justify-center mt-4">
+                    <svg viewBox="0 0 200 120" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-64 w-auto bg-gray-100 dark:bg-gray-900">
+                        <!-- Texto "IoT" con líneas de los bordes y el doble de tamaño -->
+                        <text x="50%" y="50%" text-anchor="middle" alignment-baseline="middle" font-size="8em" fill="none" stroke="#FF2D20" stroke-width="4">IoT</text>
+                    </svg>
+                </div>
+
+
+
+                {{-- <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                         <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
@@ -116,7 +125,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
 
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
